2990472c7d84f122999d5bd0df7e5aa910010af8
Brian Clymer <crdnilfan@gmail.com>
2017-04-22 16:58:01 -0500

    Running on Mac too.

--- src/components/header.js
+++ src/components/header.js
@@ -18,8 +18,8 @@ const styles = {
         height: 60,
         paddingTop: Platform.OS === 'ios' ? 15 : 0,
         shadowColor: '#000',
-        shadowOffset: { width: 0, height: 20 },
-        shadowOpacity: 0.9,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
         elevation: 2,
         position: 'relative'
     },
