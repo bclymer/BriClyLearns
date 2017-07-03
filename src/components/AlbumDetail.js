f079c24aa44001c73d39ccfc10f196077e0b1f3f
Brian Clymer <crdnilfan@gmail.com>
2017-07-03 14:33:54 -0500

    Hey I finished the app yay

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,79 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { 
+        title,
+        artist,
+        thumbnail_image,
+        image,
+        url,
+    } = album;
+    const {
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        albumImageStyle,
+    } = styles;
+
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image
+                    style={albumImageStyle}
+                    source={{ uri: image }}
+                />
+            </CardSection>
+
+            <CardSection>
+                <Button title="Buy now" onPress={() => Linking.openURL(url)} >
+                    Buy now
+                </Button>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    albumImageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    }
 };
 
 export default AlbumDetail;
